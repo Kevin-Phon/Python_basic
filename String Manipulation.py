a5b05f0dbde328b72ee4832ffe1f1a69a3ec78b4
Kevin Phon <kevinphon@Kevins-MacBook-Air.local>
2025-09-24 12:13:37 -0700

    String Manipulation

---
+++ String Manipulation.py
@@ -0,0 +1,6 @@
+#Learn to use string concatenation and the new line escape sequence to format strings in Python.
+
+print("Hello World! \n")
+print("Hello World! \n")
+print("Hello World! \n")
+print("Hello Kevin! \n")
