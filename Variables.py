5714771bb5f28a920c0c914951bdb2ecbefa7eff
Kevin Phon <kevinphon@Kevins-MacBook-Air.local>
2025-09-24 18:11:45 -0700

    Variables

---
+++ Variables.py
@@ -0,0 +1,12 @@
+# Create a greeting for your program.
+# Ask the user for the city that they grew up in and store it in a variable.
+# Ask the user for the name of a pet and store it in a variable.
+# Combine the name of their city and pet and show them their band name.
+#Make sure the input cursor shows on a new line:
+
+print("Hello World!")
+
+city_name = input("Enter your city name: ")
+pet_name = input("Enter your pet name: ")
+band_name = city_name + " " + pet_name
+print(band_name)
